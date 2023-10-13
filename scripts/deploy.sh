67f45ab04cc880e7dedc6c06d229738168b6953f
link2xt <link2xt@testrun.org>
2023-10-13 21:50:14 +0000

    Add filtermail

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 : ${CHATMAIL_DOMAIN:=c1.testrun.org}
 export CHATMAIL_DOMAIN
-cd doveauth
+
+pushd doveauth
 venv/bin/python3 -m build
-../chatmail-pyinfra/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" ../deploy.py
-rm -r dist/
+popd
+
+pushd filtermail
+venv/bin/python3 -m build
+popd
+
+chatmail-pyinfra/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" deploy.py
+
+#rm -r doveauth/dist/
+#rm -r filtermail/dist/

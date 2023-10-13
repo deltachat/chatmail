fbda0fb53c60068e7822cb9394070479e07d6558
missytake <missytake@systemli.org>
2023-10-13 20:48:36 +0200

    install doveauth system-wide via pip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 : ${CHATMAIL_DOMAIN:=c1.testrun.org}
 export CHATMAIL_DOMAIN
-chatmail-pyinfra/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" deploy.py
+cd doveauth
+venv/bin/python3 -m build
+../chatmail-pyinfra/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" ../deploy.py
+rm -r dist/
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,5 +4,5 @@ chatmail-pyinfra/venv/bin/pip install pyinfra pytest
 chatmail-pyinfra/venv/bin/pip install -e chatmail-pyinfra
 chatmail-pyinfra/venv/bin/pip install -e doveauth
 python3 -m venv doveauth/venv
-doveauth/venv/bin/pip install pytest
+doveauth/venv/bin/pip install pytest build
 doveauth/venv/bin/pip install -e doveauth

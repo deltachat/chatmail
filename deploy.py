1942ad3cef7f7f4892bad44871304db726c94c14
missytake <missytake@systemli.org>
2023-10-13 11:17:55 +0200

    ensure environment variables are set

--- deploy.py
+++ deploy.py
@@ -8,6 +8,10 @@ def main():
     mail_server = os.getenv("CHATMAIL_SERVER", mail_domain)
     dkim_selector = os.getenv("CHATMAIL_DKIM_SELECTOR", "2023")
 
+    assert mail_domain
+    assert mail_server
+    assert dkim_selector
+
     deploy_chatmail(mail_domain, mail_server, dkim_selector)
 
 

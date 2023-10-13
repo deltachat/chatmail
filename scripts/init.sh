7acd1c0db6843e17288d9e004f320ed9b881809e
missytake <missytake@systemli.org>
2023-10-13 20:15:07 +0200

    make doveauth tests pass again

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 python3 -m venv chatmail-pyinfra/venv
-chatmail-pyinfra/venv/bin/pip install pyinfra
+chatmail-pyinfra/venv/bin/pip install pyinfra pytest
 chatmail-pyinfra/venv/bin/pip install -e chatmail-pyinfra
+chatmail-pyinfra/venv/bin/pip install -e doveauth
 python3 -m venv doveauth/venv
+doveauth/venv/bin/pip install pytest
 doveauth/venv/bin/pip install -e doveauth

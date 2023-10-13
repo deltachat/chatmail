a24f1e8393c896bf989ddbe06666f1ebf6f7b553
holger krekel <holger@merlinux.eu>
2023-10-13 16:20:31 +0200

    create venv in chatmail-pyinfra

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python3 -m venv venv
-venv/bin/pip install pyinfra
-venv/bin/pip install -e chatmail-pyinfra
+python3 -m venv chatmail-pyinfra/venv
+chatmail-pyinfra/venv/bin/pip install pyinfra
+chatmail-pyinfra/venv/bin/pip install -e chatmail-pyinfra

48d890ee825b6448e4cce17b48c99ee710ce2cc0
holger krekel <holger@merlinux.eu>
2023-10-15 19:14:18 +0200

    make quota work

--- scripts/get_imap_capabilities.py
+++ scripts/get_imap_capabilities.py
@@ -7,7 +7,7 @@ domain = os.environ.get("CHATMAIL_DOMAIN", "c3.testrun.org")
 print("connecting")
 conn = imaplib.IMAP4_SSL(domain)
 print("logging in")
-conn.login(f"measure{time.time()}", "pass")
+conn.login(f"imapcapa", "pass")
 status, res = conn.capability()
 for capa in sorted(res[0].decode().split()):
     print(capa)

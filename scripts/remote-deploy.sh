735ccbc1f2dd9d19357a3059a467108fed76bbd6
missytake <missytake@systemli.org>
2023-10-15 12:07:55 +0200

    fix: remote-deploy.sh needs python3-venv

--- scripts/remote-deploy.sh
+++ scripts/remote-deploy.sh
@@ -5,4 +5,4 @@ set -e
 : ${CHATMAIL_SSH_HOST:=$CHATMAIL_DOMAIN}
 
 rsync -avz . "root@$CHATMAIL_SSH_HOST:/root/chatmail" --exclude='/.git' --filter="dir-merge,- .gitignore"
-ssh "root@$CHATMAIL_SSH_HOST" "cd /root/chatmail; python3 -m venv venv; venv/bin/pip install pyinfra build; venv/bin/python3 -m build -n --sdist doveauth --outdir dist; venv/bin/python3 -m build -n --sdist filtermail --outdir dist; venv/bin/pip install -e ./chatmail-pyinfra -e ./doveauth; export CHATMAIL_DOMAIN=$CHATMAIL_DOMAIN; venv/bin/pyinfra @local deploy.py"
+ssh "root@$CHATMAIL_SSH_HOST" "cd /root/chatmail; apt install -y python3-venv; python3 -m venv venv; venv/bin/pip install pyinfra build; venv/bin/python3 -m build -n --sdist doveauth --outdir dist; venv/bin/python3 -m build -n --sdist filtermail --outdir dist; venv/bin/pip install -e ./chatmail-pyinfra -e ./doveauth; export CHATMAIL_DOMAIN=$CHATMAIL_DOMAIN; venv/bin/pyinfra @local deploy.py"

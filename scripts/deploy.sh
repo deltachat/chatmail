262eb36a5c624880ebc51bcfe0d6dc4f2fd4c596
link2xt <link2xt@testrun.org>
2023-10-15 15:40:06 +0000

    Rename chatmail-pyinfra into deploy-chatmail

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@
 : ${CHATMAIL_DOMAIN:=c1.testrun.org}
 export CHATMAIL_DOMAIN
 
-venv/bin/python3 -m build --sdist doveauth --outdir dist
-venv/bin/python3 -m build --sdist filtermail --outdir dist
+venv/bin/python3 -m build -n --sdist doveauth --outdir dist
+venv/bin/python3 -m build -n --sdist filtermail --outdir dist
 
-chatmail-pyinfra/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" deploy.py
+deploy-chatmail/venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" deploy.py
 
 rm -r dist/
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-python3 -m venv chatmail-pyinfra/venv
-chatmail-pyinfra/venv/bin/pip install pyinfra pytest
-chatmail-pyinfra/venv/bin/pip install -e chatmail-pyinfra
-chatmail-pyinfra/venv/bin/pip install -e doveauth
+python3 -m venv deploy-chatmail/venv
+deploy-chatmail/venv/bin/pip install pyinfra pytest
+deploy-chatmail/venv/bin/pip install -e deploy-chatmail
+deploy-chatmail/venv/bin/pip install -e doveauth
 
 python3 -m venv doveauth/venv
 doveauth/venv/bin/pip install pytest

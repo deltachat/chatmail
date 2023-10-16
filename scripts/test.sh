408da296f1c2ada267273aafd97c9aad28334650
link2xt <link2xt@testrun.org>
2023-10-16 20:13:41 +0000

    test.sh: do not run slow tests by default

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ pushd chatmaild/src/chatmaild
 ../../venv/bin/pytest
 popd
 
-online-tests/venv/bin/pytest online-tests/ -vrx --durations=5 --slow
+online-tests/venv/bin/pytest online-tests/ -vrx --durations=5

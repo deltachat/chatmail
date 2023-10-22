732fdb3dab75bf0fb71202085fedafceaf9becd5
link2xt <link2xt@testrun.org>
2023-10-22 11:47:40 +0000

    Setup `deltachat` dependency in init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -3,6 +3,6 @@ set -e
 python3 -m venv venv
 pip=venv/bin/pip
 
-$pip install pyinfra pytest build 'setuptools>=68' tox 
+$pip install pyinfra pytest build 'setuptools>=68' tox deltachat
 $pip install -e deploy-chatmail 
 $pip install -e chatmaild 

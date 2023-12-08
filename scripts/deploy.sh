4b9480bd2959f41e9dc78cdc6c327662663c642b
holger krekel <holger@merlinux.eu>
2023-12-08 20:34:27 +0100

    deploy chatmaild in a virtualenv to make it easier to add dependencies

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,12 +4,5 @@ echo -----------------------------------------
 echo deploying to $CHATMAIL_DOMAIN 
 echo -----------------------------------------
 
-echo WARNING: in five seconds deploy to $CHATMAIL_DOMAIN starts
-sleep 5
-
-venv/bin/python3 -m build -n --sdist chatmaild --outdir dist
-
 venv/bin/pyinfra --ssh-user root "$CHATMAIL_DOMAIN" \
     deploy-chatmail/src/deploy_chatmail/deploy.py
-
-rm -r dist/

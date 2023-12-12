6d75643bac0701ebfc25cf533a8dea6bb8e2dc32
missytake <missytake@systemli.org>
2023-12-12 15:14:52 +0100

    tests: replace make_config with example_config, add default config params

--- chatmaild/src/chatmaild/tests/test_config.py
+++ chatmaild/src/chatmaild/tests/test_config.py
@@ -1,27 +1,31 @@
 from chatmaild.config import read_config
 
 
-def test_read_config_basic(make_config):
-    config = make_config("chat.example.org")
-    assert config.mail_domain == "chat.example.org"
-    assert not config.privacy_supervisor and not config.privacy_mail
-    assert not config.privacy_pdo and not config.privacy_postal
+def test_read_config_basic(example_config):
+    assert example_config.mail_domain == "chat.example.org"
+    assert not example_config.privacy_supervisor and not example_config.privacy_mail
+    assert not example_config.privacy_pdo and not example_config.privacy_postal
 
-    inipath = config._inipath
+    inipath = example_config._inipath
     inipath.write_text(inipath.read_text().replace("60", "37"))
-    config = read_config(inipath)
-    assert config.max_user_send_per_minute == 37
-    assert config.mail_domain == "chat.example.org"
+    example_config = read_config(inipath)
+    assert example_config.max_user_send_per_minute == 37
+    assert example_config.mail_domain == "chat.example.org"
 
 
-def test_read_config_testrun(make_config):
-    config = make_config("something.testrun.org")
-    assert config.mail_domain == "something.testrun.org"
-    assert len(config.privacy_postal.split("\n")) > 1
-    assert len(config.privacy_supervisor.split("\n")) > 1
-    assert len(config.privacy_pdo.split("\n")) > 1
-    assert config.privacy_mail == "privacy@testrun.org"
-    assert config.filtermail_smtp_port == 10080
-    assert config.postfix_reinject_port == 10025
-    assert config.max_user_send_per_minute == 60
-    assert config.passthrough_recipients
+def test_read_config_testrun(example_config):
+    assert example_config.mail_domain == "something.testrun.org"
+    assert len(example_config.privacy_postal.split("\n")) > 1
+    assert len(example_config.privacy_supervisor.split("\n")) > 1
+    assert len(example_config.privacy_pdo.split("\n")) > 1
+    assert example_config.privacy_mail == "privacy@testrun.org"
+    assert example_config.filtermail_smtp_port == 10080
+    assert example_config.postfix_reinject_port == 10025
+    assert example_config.max_user_send_per_minute == 60
+    assert example_config.max_mailbox_size == "100M"
+    assert example_config.delete_mails_after == "40d"
+    assert example_config.username_min_length == 6
+    assert example_config.username_max_length == 20
+    assert example_config.password_min_length == 9
+    assert example_config.passthrough_senders
+    assert example_config.passthrough_recipients
--- chatmaild/src/chatmaild/tests/test_newmail.py
+++ chatmaild/src/chatmaild/tests/test_newmail.py
@@ -4,26 +4,24 @@ import chatmaild
 from chatmaild.newemail import create_newemail_dict, print_new_account
 
 
-def test_create_newemail_dict(make_config):
-    config = make_config("example.org")
-    ac1 = create_newemail_dict(config)
+def test_create_newemail_dict(example_config):
+    ac1 = create_newemail_dict(example_config)
     assert "@" in ac1["email"]
     assert len(ac1["password"]) >= 10
 
-    ac2 = create_newemail_dict(config)
+    ac2 = create_newemail_dict(example_config)
 
     assert ac1["email"] != ac2["email"]
     assert ac1["password"] != ac2["password"]
 
 
-def test_print_new_account(capsys, monkeypatch, maildomain, tmpdir, make_config):
-    config = make_config(maildomain)
-    monkeypatch.setattr(chatmaild.newemail, "CONFIG_PATH", str(config._inipath))
+def test_print_new_account(capsys, monkeypatch, maildomain, tmpdir, example_config):
+    monkeypatch.setattr(chatmaild.newemail, "CONFIG_PATH", str(example_config._inipath))
     print_new_account()
     out, err = capsys.readouterr()
     lines = out.split("\n")
     assert lines[0] == "Content-Type: application/json"
     assert not lines[1]
     dic = json.loads(lines[2])
-    assert dic["email"].endswith(f"@{config.mail_domain}")
+    assert dic["email"].endswith(f"@{example_config.mail_domain}")
     assert len(dic["password"]) >= 10

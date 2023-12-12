f2d32324e3bb30580f478405e32b0c05210ca3cd
missytake <missytake@systemli.org>
2023-12-12 15:29:35 +0100

    tests: use something.testrun.org instead of chat.example.org for 1 test

--- chatmaild/src/chatmaild/tests/test_config.py
+++ chatmaild/src/chatmaild/tests/test_config.py
@@ -13,19 +13,20 @@ def test_read_config_basic(example_config):
     assert example_config.mail_domain == "chat.example.org"
 
 
-def test_read_config_testrun(example_config):
-    assert example_config.mail_domain == "something.testrun.org"
-    assert len(example_config.privacy_postal.split("\n")) > 1
-    assert len(example_config.privacy_supervisor.split("\n")) > 1
-    assert len(example_config.privacy_pdo.split("\n")) > 1
-    assert example_config.privacy_mail == "privacy@testrun.org"
-    assert example_config.filtermail_smtp_port == 10080
-    assert example_config.postfix_reinject_port == 10025
-    assert example_config.max_user_send_per_minute == 60
-    assert example_config.max_mailbox_size == "100M"
-    assert example_config.delete_mails_after == "40d"
-    assert example_config.username_min_length == 6
-    assert example_config.username_max_length == 20
-    assert example_config.password_min_length == 9
-    assert example_config.passthrough_senders
-    assert example_config.passthrough_recipients
+def test_read_config_testrun(make_config):
+    config = make_config("something.testrun.org")
+    assert config.mail_domain == "something.testrun.org"
+    assert len(config.privacy_postal.split("\n")) > 1
+    assert len(config.privacy_supervisor.split("\n")) > 1
+    assert len(config.privacy_pdo.split("\n")) > 1
+    assert config.privacy_mail == "privacy@testrun.org"
+    assert config.filtermail_smtp_port == 10080
+    assert config.postfix_reinject_port == 10025
+    assert config.max_user_send_per_minute == 60
+    assert config.max_mailbox_size == "100M"
+    assert config.delete_mails_after == "40d"
+    assert config.username_min_length == 6
+    assert config.username_max_length == 20
+    assert config.password_min_length == 9
+    assert config.passthrough_recipients == ["privacy@testrun.org"]
+    assert config.passthrough_senders == []

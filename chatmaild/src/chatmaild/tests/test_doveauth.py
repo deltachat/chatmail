98fd4b61c9b7fdc97b735ad8107564d1137c6f72
missytake <missytake@systemli.org>
2023-12-12 15:14:52 +0100

    tests: replace make_config with example_config, add default config params

--- chatmaild/src/chatmaild/tests/test_doveauth.py
+++ chatmaild/src/chatmaild/tests/test_doveauth.py
@@ -9,32 +9,37 @@ from chatmaild.doveauth import get_user_data, lookup_passdb, handle_dovecot_requ
 from chatmaild.database import DBError
 
 
-def test_basic(db, make_config):
-    config = make_config("c1.testrun.org")
-    lookup_passdb(db, config, "link2xt@c1.testrun.org", "Pieg9aeToe3eghuthe5u")
-    data = get_user_data(db, "link2xt@c1.testrun.org")
+def test_basic(db, example_config):
+    lookup_passdb(
+        db, example_config, "link2xt@chat.example.org", "Pieg9aeToe3eghuthe5u"
+    )
+    data = get_user_data(db, "link2xt@chat.example.org")
     assert data
-    data2 = lookup_passdb(db, config, "link2xt@c1.testrun.org", "Pieg9aeToe3eghuthe5u")
+    data2 = lookup_passdb(
+        db, example_config, "link2xt@chat.example.org", "Pieg9aeToe3eghuthe5u"
+    )
     assert data == data2
 
 
-def test_dont_overwrite_password_on_wrong_login(db, make_config):
+def test_dont_overwrite_password_on_wrong_login(db, example_config):
     """Test that logging in with a different password doesn't create a new user"""
-    config = make_config("something.org")
-    res = lookup_passdb(db, config, "newuser1@something.org", "kajdlkajsldk12l3kj1983")
+    res = lookup_passdb(
+        db, example_config, "newuser1@chat.example.org", "kajdlkajsldk12l3kj1983"
+    )
     assert res["password"]
-    res2 = lookup_passdb(db, config, "newuser1@something.org", "kajdlqweqwe")
+    res2 = lookup_passdb(db, example_config, "newuser1@chat.example.org", "kajdlqweqwe")
     # this function always returns a password hash, which is actually compared by dovecot.
     assert res["password"] == res2["password"]
 
 
-def test_nocreate_file(db, monkeypatch, tmpdir, make_config):
-    config = make_config("something.org")
+def test_nocreate_file(db, monkeypatch, tmpdir, example_config):
     p = tmpdir.join("nocreate")
     p.write("")
     monkeypatch.setattr(chatmaild.doveauth, "NOCREATE_FILE", str(p))
-    lookup_passdb(db, config, "newuser1@something.org", "zequ0Aimuchoodaechik")
-    assert not get_user_data(db, "newuser1@something.org")
+    lookup_passdb(
+        db, example_config, "newuser1@chat.example.org", "zequ0Aimuchoodaechik"
+    )
+    assert not get_user_data(db, "newuser1@chat.example.org")
 
 
 def test_db_version(db):
@@ -48,34 +53,32 @@ def test_too_high_db_version(db):
         db.ensure_tables()
 
 
-def test_handle_dovecot_request(db, make_config):
-    config = make_config("c3.testrun.org")
+def test_handle_dovecot_request(db, example_config):
     msg = (
         "Lshared/passdb/laksjdlaksjdlaksjdlk12j3l1k2j3123/"
-        "some42@c3.testrun.org\tsome42@c3.testrun.org"
+        "some42@chat.example.org\tsome42@chat.example.org"
     )
-    res = handle_dovecot_request(msg, db, config)
+    res = handle_dovecot_request(msg, db, example_config)
     assert res
     assert res[0] == "O" and res.endswith("\n")
     userdata = json.loads(res[1:].strip())
-    assert userdata["home"] == "/home/vmail/some42@c3.testrun.org"
+    assert userdata["home"] == "/home/vmail/some42@chat.example.org"
     assert userdata["uid"] == userdata["gid"] == "vmail"
     assert userdata["password"].startswith("{SHA512-CRYPT}")
 
 
 def test_50_concurrent_lookups_different_accounts(
-    db, gencreds, make_config, maildomain
+    db, gencreds, example_config, maildomain
 ):
     num_threads = 50
     req_per_thread = 5
     results = queue.Queue()
-    config = make_config(maildomain)
 
     def lookup(db):
         for i in range(req_per_thread):
             addr, password = gencreds()
             try:
-                lookup_passdb(db, config, addr, password)
+                lookup_passdb(db, example_config, addr, password)
             except Exception:
                 results.put(traceback.format_exc())
             else:

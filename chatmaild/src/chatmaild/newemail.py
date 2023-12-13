0fd4dfab414bade286d90714271049aaa2c3d2a8
Floris Bruynooghe <flub@devork.be>
2023-12-13 20:58:10 +0100

    Use more characters for passwords
    This expands the character set used for passwords generated for new
    accounts.  The set it taken from the set used by the pass tool.  The
    special characters is the full GNU grep [:punct:] set.

--- chatmaild/src/chatmaild/newemail.py
+++ chatmaild/src/chatmaild/newemail.py
@@ -8,12 +8,13 @@ import random
 from chatmaild.config import read_config, Config
 
 CONFIG_PATH = "/usr/local/lib/chatmaild/chatmail.ini"
+ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz1234567890"
+ALPHANUMERIC_PUNCT = r"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!#$%&'()*+,-./:;<=>?@[\]^_`{|}~." + r'"'
 
 
 def create_newemail_dict(config: Config):
-    alphanumeric = "abcdefghijklmnopqrstuvwxyz1234567890"
-    user = "".join(random.choices(alphanumeric, k=config.username_min_length))
-    password = "".join(random.choices(alphanumeric, k=config.password_min_length + 3))
+    user = "".join(random.choices(ALPHANUMERIC, k=config.username_min_length))
+    password = "".join(random.choices(ALPHANUMERIC_PUNCT, k=config.password_min_length + 3))
     return dict(email=f"{user}@{config.mail_domain}", password=f"{password}")
 
 

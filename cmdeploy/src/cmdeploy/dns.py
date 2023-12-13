2473b99dda74716b093b552eb8dfb5a542b5cce3
missytake <missytake@systemli.org>
2023-12-13 02:54:10 +0100

    DNS: re-use HTTP session to reduce query time by 7 seconds

--- cmdeploy/src/cmdeploy/dns.py
+++ cmdeploy/src/cmdeploy/dns.py
@@ -12,48 +12,50 @@ dns_types = {
 }
 
 
-def get(typ: str, domain: str) -> str:
-    """Get a DNS entry"""
-    r = requests.get(
-        url,
-        params={"name": domain, "type": typ},
-        headers={"accept": "application/dns-json"},
-    )
+class DNS:
+    def __init__(self):
+        self.session = requests.Session()
 
-    j = r.json()
-    if "Answer" in j:
-        for answer in j["Answer"]:
-            if answer["type"] == dns_types[typ]:
-                return answer["data"]
-    return ""
+    def get(self, typ: str, domain: str) -> str:
+        """Get a DNS entry"""
+        r = self.session.get(
+            url,
+            params={"name": domain, "type": typ},
+            headers={"accept": "application/dns-json"},
+        )
 
+        j = r.json()
+        if "Answer" in j:
+            for answer in j["Answer"]:
+                if answer["type"] == dns_types[typ]:
+                    return answer["data"]
+        return ""
 
-def resolve_mx(domain: str) -> (str, str):
-    """Resolve an MX entry"""
-    r = requests.get(
-        url,
-        params={"name": domain, "type": "MX"},
-        headers={"accept": "application/dns-json"},
-    )
+    def resolve_mx(self, domain: str) -> (str, str):
+        """Resolve an MX entry"""
+        r = self.session.get(
+            url,
+            params={"name": domain, "type": "MX"},
+            headers={"accept": "application/dns-json"},
+        )
 
-    j = r.json()
-    if "Answer" in j:
-        result = (0, None)
-        for answer in j["Answer"]:
-            if answer["type"] == dns_types["MX"]:
-                prio, server_name = answer["data"].split()
-                if int(prio) > result[0]:
-                    result = (int(prio), server_name)
+        j = r.json()
+        if "Answer" in j:
+            result = (0, None)
+            for answer in j["Answer"]:
+                if answer["type"] == dns_types["MX"]:
+                    prio, server_name = answer["data"].split()
+                    if int(prio) > result[0]:
+                        result = (int(prio), server_name)
+            return result
+        return None, None
+
+    def resolve(self, domain: str) -> str:
+        result = self.get("A", domain)
+        if not result:
+            result = self.get("CNAME", domain)
+            if result:
+                result = self.get("A", result[:-1])
+                if not result:
+                    result = self.get("AAAA", domain)
         return result
-    return None, None
-
-
-def resolve(domain: str) -> str:
-    result = get("A", domain)
-    if not result:
-        result = get("CNAME", domain)
-        if result:
-            result = get("A", result[:-1])
-            if not result:
-                result = get("AAAA", domain)
-    return result

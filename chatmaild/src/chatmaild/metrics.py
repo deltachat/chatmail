359c195419cfc64993b3e5f2d1167441fb21d688
holger krekel <holger@merlinux.eu>
2023-12-16 16:47:46 +0100

    count ci accounts correctly

--- chatmaild/src/chatmaild/metrics.py
+++ chatmaild/src/chatmaild/metrics.py
@@ -4,14 +4,16 @@ import time
 import sys
 
 
-def main():
-    vmail_dir = sys.argv[1]
+def main(vmail_dir=None):
+    if vmail_dir is None:
+        vmail_dir = sys.argv[1]
+
     accounts = 0
     ci_accounts = 0
 
     for path in Path(vmail_dir).iterdir():
         accounts += 1
-        if path.name.startswith("ci-"):
+        if path.name[:3] in ("ci-", "ac_"):
             ci_accounts += 1
 
     timestamp = int(time.time() * 1000)

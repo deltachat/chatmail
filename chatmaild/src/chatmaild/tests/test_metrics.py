a994a1b950fcc9319151ec5b8a013193b0c56492
holger krekel <holger@merlinux.eu>
2023-12-16 16:47:46 +0100

    count ci accounts correctly

---
+++ chatmaild/src/chatmaild/tests/test_metrics.py
@@ -0,0 +1,16 @@
+from chatmaild.metrics import main
+
+
+def test_main(tmp_path, capsys):
+    for x in ("ci-asllkj", "ac_12l3kj", "qweqwe", "ci-l1k2j31l2k3"):
+        tmp_path.joinpath(x).mkdir()
+    main(tmp_path)
+    out, _ = capsys.readouterr()
+    d = {}
+    for line in out.split("\n"):
+        if line.strip():
+            name, num, _ = line.split()
+            d[name] = int(num)
+
+    assert d["accounts"] == 4
+    assert d["ci_accounts"] == 3

f6635c86011aafb57d38878f14beefb3cd146512
link2xt <link2xt@testrun.org>
2023-12-21 19:14:20 +0000

    test: test scanning QR code

--- cmdeploy/src/cmdeploy/tests/online/test_0_qr.py
+++ cmdeploy/src/cmdeploy/tests/online/test_0_qr.py
@@ -14,3 +14,12 @@ def test_fastcgi_working(maildomain, chatmail_config):
     res = requests.post(url)
     assert maildomain in res.json().get("email")
     assert len(res.json().get("password")) > chatmail_config.password_min_length
+
+
+def test_newemail_configure(maildomain, rpc):
+    """Test configuring accounts by scanning a QR code works."""
+    url = f"DCACCOUNT:https://{maildomain}/cgi-bin/newemail.py"
+    for i in range(3):
+        account_id = rpc.add_account()
+        rpc.set_config_from_qr(account_id, url)
+        rpc.configure(account_id)

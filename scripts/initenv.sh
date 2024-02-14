e331659851e2b4a8101f8679d9aa70b3f656e6fc
missytake <missytake@systemli.org>
2024-02-14 10:36:27 +0100

    scripts: enable socks5 proxy

--- scripts/initenv.sh
+++ scripts/initenv.sh
@@ -2,5 +2,10 @@
 set -e
 python3 -m venv --upgrade-deps venv
 
-venv/bin/pip install -e chatmaild 
-venv/bin/pip install -e cmdeploy
+if [ -z ${SOCKS5_PROXY+x} ]; then
+    venv/bin/pip install -e chatmaild
+    venv/bin/pip install -e cmdeploy
+else
+    venv/bin/pip install --proxy socks5://$SOCKS5_PROXY -e chatmaild
+    venv/bin/pip install --proxy socks5://$SOCKS5_PROXY -e cmdeploy
+fi

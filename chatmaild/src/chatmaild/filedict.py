5d5e2b199c132e8c937ede91350d1c7bee1a971f
holger krekel <holger@merlinux.eu>
2024-03-27 18:32:40 +0100

    remove timeout support, it's not needed

--- chatmaild/src/chatmaild/filedict.py
+++ chatmaild/src/chatmaild/filedict.py
@@ -8,26 +8,21 @@ from contextlib import contextmanager
 class FileDict:
     """Concurrency-safe multi-reader-single-writer Persistent Dict."""
 
-    def __init__(self, path, timeout=5.0):
+    def __init__(self, path):
         self.path = path
         self.lock_path = path.with_name(path.name + ".lock")
-        self.timeout = timeout
 
     @contextmanager
     def modify(self):
-        try:
-            with filelock.FileLock(self.lock_path, timeout=self.timeout):
-                data = self.read()
-                yield data
-                write_path = self.path.with_suffix(".tmp")
-                with write_path.open("wb") as f:
-                    marshal.dump(data, f)
-                os.rename(write_path, self.path)
-        except filelock.Timeout:
-            logging.warning("could not obtain lock, removing: %r", self.lock_path)
-            os.remove(self.lock_path)
-            with self.modify() as d:
-                yield d
+        # the OS will release the lock if the process dies,
+        # and the contextmanager will otherwise guarantee release
+        with filelock.FileLock(self.lock_path):
+            data = self.read()
+            yield data
+            write_path = self.path.with_suffix(".tmp")
+            with write_path.open("wb") as f:
+                marshal.dump(data, f)
+            os.rename(write_path, self.path)
 
     def read(self):
         try:

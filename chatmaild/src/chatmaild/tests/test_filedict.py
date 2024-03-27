277465462e1b848f1ade9ede959afc10728b3997
holger krekel <holger@merlinux.eu>
2024-03-27 18:32:40 +0100

    remove timeout support, it's not needed

--- chatmaild/src/chatmaild/tests/test_filedict.py
+++ chatmaild/src/chatmaild/tests/test_filedict.py
@@ -12,18 +12,6 @@ def test_basic(tmp_path):
     assert new["456"] == 4.2
 
 
-def test_dying_lock(tmp_path, caplog):
-    fdict1 = FileDict(tmp_path.joinpath("metadata"))
-    fdict2 = FileDict(tmp_path.joinpath("metadata"), timeout=0.1)
-    with fdict1.modify() as d:
-        with fdict2.modify() as d2:
-            d2["1"] = "2"
-        assert "could not obtain" in caplog.records[0].msg
-        d["1"] = "3"
-    assert fdict1.read()["1"] == "3"
-    assert fdict2.read()["1"] == "3"
-
-
 def test_bad_marshal_file(tmp_path, caplog):
     fdict1 = FileDict(tmp_path.joinpath("metadata"))
     fdict1.path.write_bytes(b"l12k3l12k3l")

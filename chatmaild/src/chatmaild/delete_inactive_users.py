f1c10cac2b4b69ef0bc1b7193723141c7112958f
holger krekel <holger@merlinux.eu>
2024-07-09 23:24:28 +0200

    chunked deletion

--- chatmaild/src/chatmaild/delete_inactive_users.py
+++ chatmaild/src/chatmaild/delete_inactive_users.py
@@ -11,18 +11,19 @@ from .database import Database
 from .doveauth import iter_userdb_lastlogin_before
 
 
-def remove_user(db, config, user):
-    user_mail_dir = config.get_user_maildir(user)
-    shutil.rmtree(user_mail_dir, ignore_errors=True)
-    with db.write_transaction() as conn:
-        conn.execute("DELETE FROM users WHERE addr = ?", (user,))
-
-
-def delete_inactive_users(db, config):
+def delete_inactive_users(db, config, CHUNK=100):
     cutoff_date = time.time() - config.delete_inactive_users_after * 86400
-    for user in iter_userdb_lastlogin_before(db, cutoff_date):
-        remove_user(db, config, user)
-        print(f"Deleted user: {user}")
+
+    old_users = iter_userdb_lastlogin_before(db, cutoff_date)
+    chunks = (old_users[i : i + CHUNK] for i in range(0, len(old_users), CHUNK))
+    for sublist in chunks:
+        for user in sublist:
+            user_mail_dir = config.get_user_maildir(user)
+            shutil.rmtree(user_mail_dir, ignore_errors=True)
+
+        with db.write_transaction() as conn:
+            for user in sublist:
+                conn.execute("DELETE FROM users WHERE addr = ?", (user,))
 
 
 def main():
